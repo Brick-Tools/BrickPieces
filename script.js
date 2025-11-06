d59d8894f519f1b92c4d6a8ac94ceff85db282e9
Brick-Tools <166328365+Brick-Tools@users.noreply.github.com>
2025-11-06 02:07:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,271 @@
-fetch('image_list.json')
-  .then(res => res.json())
-  .then(imageUrls => {
-    const container = document.getElementById('cards');
-    imageUrls.forEach(url => {
-      const card = document.createElement('div');
-      card.className = 'card';
+window.onload = function() {
+  cardCount = 0
+  // getPieces(cardCount)
+  // getRowCount()
+  getPieces()
+}
 
-      const img = document.createElement('img');
-      img.src = url;
-      img.alt = '';
+const supabaseUrl = 'https://jbqrtvchsaonsmpwsjcb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpicXJ0dmNoc2FvbnNtcHdzamNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNjg4MTQsImV4cCI6MjA0MjY0NDgxNH0.KGIZTN_Dm1Z_8G_uMnUCto-7eVLDH0IgUaG8oUwMwu8';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-      const title = document.createElement('h1');
-      id = url.split('\\')[1].split('-')[0]
-    //   title.textContent = id
+pieces = []
+// piecesList = []
+async function getPieces() {
+  // get number of rows in table
+  // const { count:numRows, errorCount } = await supabase
+    // .from('brickPieces')
+  //   .select('*', { count: 'exact', head: true }) // head: true avoids fetching all data
+  //   .not("latest_year", "is", null)
+  // if (errorCount) {
+  //   console.error(errorCount)
+  //   return
+  // }
+  // console.log('Number of rows:', numRows)
 
-      const link = document.createElement('a');
-      link.textContent = id
-      link.href = `https://rebrickable.com/parts/${id}`
-      link.target = "_blank"
-      title.appendChild(link)
+  // cardCount = 0
+
+  // load first 20 to make loading time feel quicker
+  const { data: response0, error0 } = await supabase
+    .from("brickPiecesStart")
+    .select("*")
+    // .not("latest_year", "is", null)
+    // .order("latest_year", { ascending: false })
+    // .order("id", { ascending: true })
+    // .range(0, 19)
+    console.log("db response0:", response0)
+      // cardCount +=20
+      if (error0) {
+        console.error("Error fetching db data:", error)
+      }
+      response0.forEach(part => {
+        pieces.push(part)
+      })
+
+      makeCards(pieces)
 
 
-      card.appendChild(img);
-      card.appendChild(title);
-    //   card.appendChild(link);
-      container.appendChild(card);
-    });
+  // load all remaining pieces
+  const { data: response, error } = await supabase
+  .from("brickPiecesRemain")
+  .select("*")
+  // .not("latest_year", "is", null)
+  // .order("latest_year", { ascending: false })
+  // .order("id", { ascending: true })
+  // .range(20, 3000 )
+  
+  console.log("db response:", response)
+    
+  
+    if (error) {
+      console.error("Error fetching db data:", error)
+    }
+    response.forEach(part => {
+      pieces.push(part)
+      // console.log(part["part"]);
+      
+      // piecesList[part["part"]]={"year":part["latest_year"], "img":`piecesNew\\${part["part"]}.webp`, "altIDs":[], "categories":[], "names":[]}
+    })
+  
+    // // pieces = response
+    // // console.log("pieces:", pieces)
+
+
+    makeCards(pieces)
+    
+    return 0
+}
+
+
+function makeCards(pieces) {
+  document.getElementById("cards").innerHTML = "";
+  const container = document.getElementById('cards');
+  pieces.forEach(key =>  {    
+    // url = key["img"]
+    url = `pieces\\${key["part"]}.webp`
+    const card = document.createElement('div');
+        card.className = 'card';
+
+        if (key["latest_year"] != 2025) {
+          card.className = 'card retired';
+        }
+        card.id=key["part"]
+  
+        const img = document.createElement('img');
+        // img.width = "1080"
+        // img.height = "1080"
+        img.src = url;
+        img.alt = '';
+  
+        const info = document.createElement('div');
+        // info.classList.add("inline-edge")
+        info.classList.add("info")
+        const title = document.createElement('h1');
+        const subTitle = document.createElement('h1');
+        subTitle.className = "copy"
+        const details = document.createElement('details')
+        const summary = document.createElement('summary')
+        // what was this + button for?
+        // const add = document.createElement('h1');
+        // add.innerText="+";
+        // add.classList.add("pointer")
+        // add.classList.add("no-select")
+        const addInput = document.createElement('input');
+        addInput.type="text";
+        addInput.placeholder = "Suggest a name or category"
+        addInput.addEventListener("keypress", function(event) {
+          if (event.key === 'Enter') {
+            console.log(this.parentElement.parentElement)
+            data = {
+              "element": this.parentElement.parentElement.id,
+              "name": this.value
+            }
+            appendRow( "brickPieces", data)
+            this.value=""
+          }
+        })
+
+        id = url.split('\\')[1].split('.')[0] //+"element"
+        
+        if (key["names"] == null) { partName = id }
+        else {partName = key["names"][0]}
+
+        title.title = partName
+        subTitle.innerText = id
+        subTitle.title = "Click to copy"
+
+        // copy part ID on click
+        subTitle.addEventListener("click", function () {
+          navigator.clipboard.writeText(this.innerText);        
+        })
+  
+        const link = document.createElement('a');
+        link.textContent = partName
+        // console.log(id);
+        
+        link.href = `https://rebrickable.com/parts/${id}`
+        link.target = "_blank"
+        title.appendChild(link)
+        info.appendChild(title)
+        // info.appendChild(add)
+        info.appendChild(subTitle)
+  
+        // MARK: details
+        summary.innerText="Full Details"
+        // details.innerText="details"
+        details.appendChild(Object.assign(document.createElement("hr")));
+        details.appendChild(Object.assign(document.createElement("p"), { innerText: `Part ID: ${key["part"]}` }));
+        details.appendChild(Object.assign(document.createElement("p"), { innerText: `Latest Year Produced: ${key["latest_year"]}` }));
+        details.appendChild(Object.assign(document.createElement("p"), { innerText: "Names:" }));
+        namesList = document.createElement("ul")
+        key["names"].forEach( name => { namesList.appendChild(Object.assign(document.createElement("li"), { innerText: name })); })
+        details.appendChild(namesList)
+
+        details.appendChild(summary)
+
+        info.appendChild(addInput); //
+        info.appendChild(details)
+
+        card.appendChild(img);
+        card.appendChild(info); //
+      //   card.appendChild(link);
+        container.appendChild(card);
+  });  
+}
+
+// adding rows to db
+async function appendRow(table, data) {
+  // console.log(`Added to table "${table}":`, data)
+  // message = `Submitted name "${data["name"]}" for ${data["element"]} for review`
+  // toast(message)
+  // return
+
+  const { error } = await supabase
+  .from('pending')
+  .insert({ table: table, data:data })
+  if (error) {
+    console.error('Error fetching data:', error);
+  } else {
+    message = `Submitted name "${data["name"]}" for ${data["element"]} for review`
+    console.log(message)
+    toast(message)
+  }
+}
+function toast(msg, duration = 4000) {
+  console.log("toast:", msg);
+  const t = Object.assign(document.createElement("div"), {
+    textContent: msg,
+    style: `
+      position: fixed; top: 20px; left: 50%;
+      transform: translateX(-50%);
+      background: rgba(0,0,0,0.8); color: #fff;
+      padding: 8px 16px; border-radius: 8px;
+      font-size: 14px; z-index: 9999;
+      opacity: 1; transition: opacity 0.5s;
+    `
   });
+  document.body.appendChild(t);
+  setTimeout(() => (t.style.opacity = 0), duration);
+  setTimeout(() => t.remove(), duration + 500);
+}
+
+
+search = ""
+document.getElementById("search").addEventListener("input", function(event) {
+  search = event.target.value;
+  filter();
+})
+
+function checkMatch(string, searchTerm, searchWhole) {
+  // console.log(searchWhole, searchTerm, string);
+  if( searchWhole ) {boundaryRegex = new RegExp(`\\b${searchTerm}\\b`, "i")}
+  else {boundaryRegex = new RegExp(`\\b${searchTerm}`, "i")}
+
+  return boundaryRegex.test(string);
+}
+
+function filter() {
+  searchWhole = false
+  if (search.slice(-1) == ' ') {
+    searchWhole = true
+  }
+  search = search.toLowerCase().trim()
+  // filterResults = {}
+  filterResults = []
+
+
+  pieces.forEach( part => {
+    piece = part["part"]
+    
+    if (piece.includes(search)) {
+      if(!filterResults.some(d => d["part"] === piece)) {
+        // console.log("add:", piece);        
+       filterResults.push(part)
+      }
+    };
+
+    
+    part.names.forEach( name => {
+      name = name.toLowerCase().trim();
+      if (checkMatch(name, search, searchWhole)) {
+        if(!filterResults.some(d => d["part"] === piece)) {
+        filterResults.push(part)
+        }
+      }
+    })
+    
+    // piece["altIDs"].forEach(altID => {
+    //   if (altID.includes(search)) {
+    //     // console.log("altID", altID)
+    //     if(filterResults[part]==undefined) {filterResults[part] = piece}
+    //   }});
+
+    // piece["categories"].forEach(category => {
+    //   if (category.includes(search)) {
+    //     // console.log("category", category)
+    //     if(filterResults[part]==undefined) {filterResults[part] = piece}
+    //   }});      
+    })
+    // console.log(filterResults);
+    makeCards(filterResults)
+}
